45d5061ca57254cbe849dfc19e42b5a05594c164
Aaditya kumar jha <adityakumarbh46@gmail.com>
2024-07-17 00:09:58 +0530

    html addead

--- app.js
+++ app.js
@@ -4,16 +4,19 @@ const logger = require('./logger');
 const authRoutes = require('./routes/authRoutes');
 const sellerRoutes = require('./routes/sellerRoutes');
 const buyerRoutes = require('./routes/buyerRoutes');
+const path = require("path")
 
 const app = express();
 
 app.use(express.json());
-
+app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   logger.info('Incoming request: %s %s', req.method, req.url);
   next();
 });
-
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname,  'documnentaion.html'));
+});
 app.use('/auth', authRoutes);
 app.use('/seller', sellerRoutes);
 app.use('/buyer', buyerRoutes);
